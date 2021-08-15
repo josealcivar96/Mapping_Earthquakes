13766a21811f55615e3598306fd7dbf2d56f6e78
josealcivar96 <unitedav@gmail.com>
2021-08-15 17:52:09 -0400

    Adding airports JSON

---
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -0,0 +1,55 @@
+// add console.log to check to see if our code is working
+console.log("working");
+//create map object with a center and a zoom level
+let map = L.map('mapid', {
+    center: [
+      37.5, -122.5
+    ],
+    zoom: 10
+});
+
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+// Grabbing our GeoJSON data.
+L.geoJson(sanFranAirport, {
+    // We turn each feature into a marker on the map.
+    pointToLayer: function(feature, latlng) {
+      console.log(feature);
+      return L.marker(latlng).bindPopup("<h2>" + feature.properties.city + "</h2>");
+    }
+
+  }).addTo(map);
+
+//create tile layer for the background of our map
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+
+//add to the layer to the map
+streets.addTo(map);
+
+
+
